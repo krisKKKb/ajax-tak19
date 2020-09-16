fd86ec6e2ea385d87c71d98a4c7f68415653f4fb
krisKKKb <55477863+krisKKKb@users.noreply.github.com>
2020-09-16 14:01:26 +0300

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,1 +1,13 @@
 
+
+<?php
+
+require_once 'connect.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+$stmt = $pdo->prepare('DELETE FROM books WHERE id = :id');
+$stmt->execute(['id' => $id]);
+
+header('Location: ./index.php');
+?>
